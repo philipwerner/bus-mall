eeb53a25da81e5bd04afcffdce851baf7f728bb2
philipwerner <philip.r.werner@gmail.com>
2017-07-24 14:14:50 -0700

    wrote main body of js, displaying images, need to keep images from duplicating

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 'use strict';
+
+function Image(name){
+  this.name = name;
+  this.source = 'img/' + this.name + '.jpg';
+  this.timesShown = 0;
+  this.timesClicked = 0;
+  Image.all.push(this);
+};
+
+Image.all = [];
+Image.allNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+for(var i = 0; i < Image.allNames.length; i++){
+  new Image(Image.allNames[i]);
+};
+
+Image.imgEl1 = document.getElementById('image1');
+Image.imgEl2 = document.getElementById('image2');
+Image.imgEl3 = document.getElementById('image3');
+
+function randomImage(){
+  var randomIndex = Math.floor(Math.random() * Image.all.length);
+  Image.imgEl1.src = Image.all[randomIndex].source;
+  Image.imgEl1.alt = Image.all[randomIndex].name;
+
+  Image.imgEl2.src = Image.all[randomIndex].source;
+  Image.imgEl2.alt = Image.all[randomIndex].name;
+
+  Image.imgEl3.src = Image.all[randomIndex].source;
+  Image.imgEl3.alt = Image.all[randomIndex].name;
+
+  Image.all[randomIndex].timeShown += 1;
+  console.log(Image.all[randomIndex].name + ' has been shown ' + Image.all[randomindex].timesShown + '.');
+};
+
+document.getElementById('image1').addEventListener('click', randomImage);
+document.getElementById('image2').addEventListener('click', randomImage);
+document.getElementById('image3').addEventListener('click', randomImage);
+randomImage();
