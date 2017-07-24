ee5852ff1a808830ddd84a40988b4a6aeecdcde9
philipwerner <philip.r.werner@gmail.com>
2017-07-24 16:47:27 -0700

    crap is jacked

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ function Image(name){
   Image.all.push(this);
 };
 
+var timesClicked1 = 0;
+var timesClicked2 = 0;
+var timesClicked3 = 0;
+
 Image.all = [];
 Image.allNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
@@ -35,18 +39,15 @@ function randomImage2(){
   Image.imgEl2.src = Image.all[randomIndex].source;
   Image.imgEl2.alt = Image.all[randomIndex].name;
 
-
-  Image.all[randomIndex].timesShown += 1;
-  console.log(Image.all[randomIndex].name + ' has been shown ' + Image.all[randomIndex].timesShown + '.');
-
+  if(Image.imgEl2 === Image.imgEl1){
+    console.log('re-roll');
+    Image.imgEl2.src = Image.all[randomIndex].source;
+    Image.imgEl2.alt = Image.all[randomIndex].name;
+  } else {
+    Image.all[randomIndex].timesShown += 1;
+    console.log(Image.all[randomIndex].name + ' has been shown ' + Image.all[randomIndex].timesShown + '.');
+  };
 }
-
-// if(randomImage2() === randomImage()){
-//   console.log('re-roll');
-//   randomImage2();
-// } else {
-//
-// }
 
 function randomImage3(){
 
@@ -55,14 +56,36 @@ function randomImage3(){
   Image.imgEl3.src = Image.all[randomIndex].source;
   Image.imgEl3.alt = Image.all[randomIndex].name;
 
-  Image.all[randomIndex].timesShown += 1;
-  console.log(Image.all[randomIndex].name + ' has been shown ' + Image.all[randomIndex].timesShown + '.');
+  if(Image.imgEl3 === Image.imgEl1 || Image.imgEl3 === Image.imgEl2){
+    console.log('re-roll');
+    Image.imgEl3.src = Image.all[randomIndex].source;
+    Image.imgEl3.alt = Image.all[randomIndex].name;
+  } else {
+    Image.all[randomIndex].timesShown += 1;
+    console.log(Image.all[randomIndex].name + ' has been shown ' + Image.all[randomIndex].timesShown + '.');
+  };
 
+}
+
+function onClick1(){
+  timesClicked1 += 1;
+  console.log('1 has been clicked ' + timesClicked1 + '.');
+}
+function onClick2(){
+  timesClicked2 += 1;
+  console.log('2 has been clicked ' + timesClicked2 + '.');
+}
+function onClick3(){
+  timesClicked3 += 1;
+  console.log('3 has been clicked ' + timesClicked3 + '.');
 }
 
 document.getElementById('products').addEventListener('click', randomImage);
 document.getElementById('products').addEventListener('click', randomImage2);
 document.getElementById('products').addEventListener('click', randomImage3);
+document.getElementById('image1').addEventListener('click', onClick1);
+document.getElementById('image2').addEventListener('click', onClick2);
+document.getElementById('image3').addEventListener('click', onClick3);
 randomImage();
 randomImage2();
 randomImage3();
