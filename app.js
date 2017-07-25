de568d13059a72f638f078ca548286829c391084
philipwerner <philip.r.werner@gmail.com>
2017-07-25 11:01:29 -0700

    class build added

--- app.js
+++ app.js
@@ -20,57 +20,71 @@ Image.imgEl2 = document.getElementById('image2');
 Image.imgEl3 = document.getElementById('image3');
 
 var randomImage = function(){
-  var randomIndex = Math.floor(Math.random() * Image.all.length);
-  var randomIndex2 = Math.floor(Math.random() * Image.all.length);
-  var randomIndex3 = Math.floor(Math.random() * Image.all.length);
+  var rand1 = Math.floor(Math.random() * Image.all.length);
+  var rand2 = Math.floor(Math.random() * Image.all.length);
+  var rand3 = Math.floor(Math.random() * Image.all.length);
 
 
-  Image.imgEl1.src = Image.all[randomIndex].source;
-  Image.imgEl1.alt = Image.all[randomIndex].name;
-  Image.imgEl2.src = Image.all[randomIndex2].source;
-  Image.imgEl2.alt = Image.all[randomIndex2].name;
-  Image.imgEl3.src = Image.all[randomIndex3].source;
-  Image.imgEl3.alt = Image.all[randomIndex3].name;
+  Image.imgEl1.src = Image.all[rand1].source;
+  Image.imgEl1.alt = Image.all[rand1].name;
+
+  Image.imgEl2.src = Image.all[rand2].source;
+  Image.imgEl2.alt = Image.all[rand2].name;
+  while(rand2 === rand1){
+    console.log('duplicated image 2, re-roll');
+    rand2 = Math.floor(Math.random() * Image.all.length);
+  };
+
+  Image.imgEl3.src = Image.all[rand3].source;
+  Image.imgEl3.alt = Image.all[rand3].name;
+  while(rand3 === rand1 || rand3 === rand2){
+    console.log('duplicated image 3, re-roll');
+    rand3 = Math.floor(Math.random() * Image.all.length);
+  };
   // afterClick();
-  deDuplicate();
+  // deDuplicate();
 
-  Image.all[randomIndex].timesShown += 1;
-  console.log(Image.all[randomIndex].name + ' has been shown ' + Image.all[randomIndex].timesShown + '.');
-  Image.all[randomIndex2].timesShown += 1;
-  console.log(Image.all[randomIndex2].name + ' has been shown ' + Image.all[randomIndex2].timesShown + '.');
-  Image.all[randomIndex3].timesShown += 1;
-  console.log(Image.all[randomIndex3].name + ' has been shown ' + Image.all[randomIndex3].timesShown + '.');
+  Image.all[rand1].timesShown += 1;
+  console.log(Image.all[rand1].name + ' has been shown ' + Image.all[rand1].timesShown + '.');
+  Image.all[rand2].timesShown += 1;
+  console.log(Image.all[rand2].name + ' has been shown ' + Image.all[rand2].timesShown + '.');
+  Image.all[rand3].timesShown += 1;
+  console.log(Image.all[rand3].name + ' has been shown ' + Image.all[rand3].timesShown + '.');
 
 };
 
-function deDuplicate (){
-  if(Image.imgEl1.src === Image.imgEl2.src || Image.imgEl1.src === Image.imgEl3.src){
-    console.log('re-roll 1');
-    randomImage();
-  } else if(Image.imgEl3.src === Image.imgEl2.src) {
-    console.log('re-roll 2');
-    randomImage();
-  };
+function eventHandler(){
+  getRandoms();
 }
+//
+// function deDuplicate (){
+//   if(Image.imgEl1.src === Image.imgEl2.src || Image.imgEl1.src === Image.imgEl3.src){
+//     console.log('re-roll 1');
+//     randomImage();
+//   } else if(Image.imgEl3.src === Image.imgEl2.src) {
+//     console.log('re-roll 2');
+//     randomImage();
+//   };
+// }
 
 function getRandoms(){
   randomImage();
   // randomImage2();
   // randomImage3();
-  afterClick();
+  // afterClick();
 }
-function afterClick (){
-  if(Image.imgEl1.src === Image.imgEl1.src || Image.imgEl1.src === Image.imgEl2.src || Image.imgEl1.src === Image.imgEl3.src || Image.imgEl2.src === Image.imgEl2.src || Image.imgEl2.src === Image.imgEl3.src || Image.imgEl3.src === Image.imgEl3.src){
-    console.log('re-roll after clicked');
-    // getRandoms();
-  // } else if(Image.imgEl2.src === Image.imgEl1.src || Image.imgEl2.src === Image.imgEl2.src || Image.imgEl2.src === Image.imgEl3.src) {
-  //   console.log('re-roll 2 after clicked');
-  //   randomImage2();
-  // } else if(Image.imgEl3.src === Image.imgEl1.src || Image.imgEl3.src === Image.imgEl2.src || Image.imgEl3.src === Image.imgEl3.src){
-  //   console.log('re-roll 3 after clicked');
-  //   randomImage3();
-  }
-}
+// function afterClick (){
+//   if(Image.imgEl1.src === Image.imgEl1.src || Image.imgEl1.src === Image.imgEl2.src || Image.imgEl1.src === Image.imgEl3.src || Image.imgEl2.src === Image.imgEl2.src || Image.imgEl2.src === Image.imgEl3.src || Image.imgEl3.src === Image.imgEl3.src){
+//     console.log('re-roll after clicked');
+//     // getRandoms();
+//   // } else if(Image.imgEl2.src === Image.imgEl1.src || Image.imgEl2.src === Image.imgEl2.src || Image.imgEl2.src === Image.imgEl3.src) {
+//   //   console.log('re-roll 2 after clicked');
+//   //   randomImage2();
+//   // } else if(Image.imgEl3.src === Image.imgEl1.src || Image.imgEl3.src === Image.imgEl2.src || Image.imgEl3.src === Image.imgEl3.src){
+//   //   console.log('re-roll 3 after clicked');
+//   //   randomImage3();
+//   }
+// }
 // function onClick(){
 //   var randomIndex = Math.floor(Math.random() * Image)
 //   Image.all[randomIndex].timesClicked += 1;
--- app2.js
+++ app2.js
@@ -1,12 +1,14 @@
 'use strict';
 
-function Image(number) {
-  this.name = number;
-  this.source = 'Images/' + this.name + '.jpg';
+function Image(name){
+  this.name = name;
+  this.source = 'img/' + this.name + '.jpg';
   this.timesShown = 0;
+  this.timesClicked = 0;
   Image.all.push(this);
-}
+};
 
+Image.totalClicks = 0;
 Image.all = [];
 Image.allNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
@@ -14,22 +16,68 @@ for(var i = 0; i < Image.allNames.length; i++){
   new Image(Image.allNames[i]);
 };
 
-// Image.imgEl = document.getElementById('image');
+Image.imgEl1 = document.getElementById('image1');
+Image.imgEl2 = document.getElementById('image2');
+Image.imgEl3 = document.getElementById('image3');
+Image.container = document.getElementById('products');
 
-function randomImage(){
-//   console.log(e.target.alt);
-//   if(e.target.alt === 'nine'){
-//     alert('OMG A BABY GOAT!!!!');
-//   }
-  // console.log(Image.imgEl.alt, 'is the image being displayed before the click.');
-  var randomIndex = Math.floor(Math.random() * Image.all.length);
-  Image.imgEl.src = Image.all[randomIndex].source;
-  Image.imgEl.alt = Image.all[randomIndex].name;
-  Image.all[randomIndex].timesShown += 1;
-  console.log(Image.all[randomIndex].name + ' has been shown ' + Image.all[randomIndex].timesShown + '.' );
-};
+function makeRandomNumber(){
+  return Math.floor(Math.random() * Image.all.length);
+}
 
-document.getElementById('image1').addEventListener('click', randomImage);
-document.getElementById('image2').addEventListener('click', randomImage);
-document.getElementById('image3').addEventListener('click', randomImage);
-randomImage();
+function displayImages(){
+  // generate 3 random numbers
+  // ensure those numbers are unique
+  var numbers = [];
+  numbers[0] = makeRandomNumber();
+  numbers[1] = makeRandomNumber();
+  while(numbers[0] === numbers[1]){
+    console.log('image 2 duped');
+    numbers[1] = makeRandomNumber();
+  }
+  numbers[2] = makeRandomNumber();
+  while(numbers[2] === numbers[1] || numbers[2] === numbers[0]){
+    console.log('image 3 duped');
+    numbers[2] = makeRandomNumber();
+  }
+  Image.imgEl1.src = Image.all[numbers[0]].source;
+  Image.imgEl2.src = Image.all[numbers[1]].source;
+  Image.imgEl3.src = Image.all[numbers[2]].source;
+  Image.imgEl1.alt = Image.all[numbers[0]].name;
+  Image.imgEl2.alt = Image.all[numbers[1]].name;
+  Image.imgEl3.alt = Image.all[numbers[2]].name;
+  Image.all[numbers[0]].timesShown += 1;
+  Image.all[numbers[1]].timesShown += 1;
+  Image.all[numbers[2]].timesShown += 1;
+}
+
+function showList(){
+  var ulEl = document.getElementById('list');
+
+  for(var i = 0; i < Image.all.length; i++){
+    var liEl = document.createElement('li');
+    liEl.textContent = Image.all[i].name + ' was shown ' + Image.all[i].timesShown + ' and was clicked ' + Image.all[i].timesClicked + ' times.';
+    ulEl.appendChild(liEl);
+  }
+}
+
+function handleClick(e){
+  Image.totalClicks += 1;
+  console.log(Image.totalClicks);
+  if(Image.totalClicks === 3){
+    Image.container.removeEventListener('click', handleClick);
+    showList();
+  }
+  console.log(e.target.alt);
+  for(var i = 0; i < Image.all.length; i++){
+    if(e.target.alt === Image.all[i].name){
+      // tally a click
+      Image.all[i].timesClicked += 1;
+    }
+  }
+
+  displayImages();
+}
+
+displayImages();
+Image.container.addEventListener('click', handleClick);
